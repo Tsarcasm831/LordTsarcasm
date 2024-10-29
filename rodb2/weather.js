9a280b992a8321517e87036de35b2816e6253780
Tsarcasm831 <cap.anton@gmail.com>
2024-10-29 17:58:41 -0600

    Update weather.js

--- rodb2/weather.js
+++ rodb2/weather.js
@@ -1,6 +1,6 @@
 // weather.js - Weather system
 
-import * as THREE from 'https://unpkg.com/three@0.152.2/build/three.module.js';
+import * as THREE from './node_modules/three/build/three.module.js';
 
 
 const PARTICLE_POOL_SIZE = 15000;
